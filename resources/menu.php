2373a0bfef3330c33a42f81e6094e6bea4e624f7
Carlolux <carlos.cg170404@gmail.com>
2024-01-02 16:50:05 -0500

    nav bar

---
+++ resources/menu.php
@@ -0,0 +1,65 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">App Trabajos de Grado</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?php echo $url;?>main.php">Inicio</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profesores
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="<?php echo $url;?>profesores/adicionar1.php">Adicionar</a></li>
+            <li><a class="dropdown-item" href="<?php echo $url;?>profesores/eliminar1.php">Eliminar</a></li>
+            <li><a class="dropdown-item" href="<?php echo $url;?>profesores/modificar1.php">Modificar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="<?php echo $url;?>profesores/listar.php">Listar</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Utilidades
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="">Crear Trabajo de Grado</a></li>
+            <li><a class="dropdown-item" href="">Asignar Estudiante</a></li>
+            <li><a class="dropdown-item" href="">Listar Trabajos</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="">Crear Comité de Evaluación</a></li>
+            <li><a class="dropdown-item" href="">Evaluar</a></li>
+            <li><a class="dropdown-item" href="">Listar evaluaciones</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Reportes
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Reporte 1</a></li>
+            <li><a class="dropdown-item" href="#">Reporte 2</a></li>
+            <li><a class="dropdown-item" href="#">Reporte 3</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class = "nav-link" aria-current = "page" href = "<?php echo $url;?>recursos/cerrar.php">Cerrar sesión</a>
+        </li>
+
+
+
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Estudiante" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
