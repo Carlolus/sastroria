45d49ce33a55b146fdfef12793be1002f90ce90e
Carlolux <carlos.cg170404@gmail.com>
2024-01-02 17:14:37 -0500

    fixes menu bug

--- main.php
+++ main.php
@@ -4,8 +4,9 @@
     <?php include("resources/header.php"); ?>
 </head>
 <body>
+    <?php include("resources/menu.php"); ?>
     <div class="container">
-        <?php include("resources/menu.php"); ?>
+        
         <h2>Hola Jhonathan</h2>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
