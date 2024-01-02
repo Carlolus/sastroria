7987b9a6d41bd3ebfae269dbd2f0614a128630cc
jhonatanjcdg <j3172224877@gmail.com>
2024-01-02 15:14:36 -0500

    xdxd

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
     <title>Sastroria</title>
 </head>
 <body>
-    <div class="row">
-        <form action="">
-            <label for="" class = "control-label">Diego</label>
+    <div class="container">
+        <form>
+            <div class="mb-6">
+                <label for="" class = "form-label">Usuario</label>
+                <input type="text" class="form-control" required>
+
+                <label for="" class = "form-label">Contraseña</label>
+                <input type="password" class="form-control" required>
+            </div>
         </form>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 </html>
