ce301d1312b9f1a3d0d40c859b893adbae081d6b
DieGod123 <diegodelgado685@gmail.com>
2024-01-02 16:01:06 -0500

    nombre nav

--- nav.php
+++ nav.php
@@ -9,7 +9,7 @@
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Mi Sitio</a>
+        <a class="navbar-brand" href="#">Sasteria</a>
         
         <!-- Menú Hamburguesa -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
