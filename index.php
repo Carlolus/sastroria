adf7802d9bdbff9486a2a681f57436fbecf306b7
DieGod123 <diegodelgado685@gmail.com>
2024-01-02 16:19:57 -0500

    login?

--- index.php
+++ index.php
@@ -1,69 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <!-- Diego Germán Delgado Martinez -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sastroria</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Taller</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <div class="row">
-        <form>
-    <!-- Email input -->
-            <div class="form-outline mb-4">
-                <input type="text" id="form2Example1" class="form-control" />
-                <label class="form-label" for="form2Example1">Email address</label>
-            </div>
 
-            <!-- Password input -->
-            <div class="form-outline mb-4">
-                <input type="password" id="form2Example2" class="form-control" />
-                <label class="form-label" for="form2Example2">Password</label>
-            </div>
+    <header class="navbar navbar-expand-lg navbar-light bg-light">
+        <span class="navbar-toggler-icon" id="barra"></span>
 
-            <!-- 2 column grid layout for inline styling -->
-            <div class="row mb-4">
-                <div class="col d-flex justify-content-center">
-                <!-- Checkbox -->
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                    <label class="form-check-label" for="form2Example31"> Remember me </label>
-                </div>
-                </div>
+        <div class="imagenlogo navbar-brand">
+            <img src="img/america-del-sur 2.png" alt="si">
+            <h2 class="logo">SoftwareLA</h2>
+        </div>
 
-                <div class="col">
-                <!-- Simple link -->
-                <a href="#!">Forgot password?</a>
-                </div>
-            </div>
+        <nav class="navbar-nav ml-auto">
+            <a href="#" class="nav-link">Inicio</a>
+            <a href="#" class="nav-link">Acerca</a>
+            <a href="#" class="nav-link">Contacto</a>
+            <a href="#" class="nav-link">Ayuda</a>
+        </nav>
+    </header>
 
-            <!-- Submit button -->
-            <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-            <!-- Register buttons -->
-            <div class="text-center">
-                <p>Not a member? <a href="#!">Register</a></p>
-                <p>or sign up with:</p>
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-facebook-f"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-google"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-twitter"></i>
-                </button>
-
-                <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-github"></i>
-                </button>
-            </div>
-        </form>
+    <div id="menuCompleto" class="menu-completo">
+        <a href="#" class="nav-link">Inicio</a>
+        <a href="#" class="nav-link">Acerca</a>
+        <a href="#" class="nav-link">Contacto</a>
+        <a href="#" class="nav-link">Ayuda</a>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <div class="container contenedor">
+
+        <div class="info">
+            <h1>¡Bienvenid@ a SoftwareLA!</h1>
+            <div class="candidatos">
+                <div class="content">
+                    <p>En SoftwareLA, entendemos que el conocimiento es la clave para desbloquear todo tu potencial. Ofrecemos una amplia gama de cursos online diseñados para desafiar y nutrir tu mente inquisitiva. Desde principiantes hasta expertos, nuestros cursos están creados para todos, sin importar en qué punto de tu viaje de aprendizaje te encuentres.</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="registro" class="mt-5">
+            <h2>¡Crea tu cuenta y obtén hasta un 20% de descuento en cursos de programación!</h2>
+            <br>
+            <form>
+                <div class="form-group">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" class="form-control" id="nombre" required autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="apellido">Apellido</label>
+                    <input type="text" class="form-control" id="apellido" required autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="correo">Correo</label>
+                    <input type="email" class="form-control" id="correo" required autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <label for="contraseña">Contraseña</label>
+                    <input type="password" class="form-control" id="contraseña" required autocomplete="off">
+                </div>
+                <button type="submit" class="btn btn-primary">Crear cuenta</button>
+                <br>
+                <a href="#" class="nav-link">¿Ya tienes una cuenta? Haz clic aquí para ingresar.</a>
+            </form>
+        </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="js/code.js"></script>
 </body>
+
 </html>
