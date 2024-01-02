f07c78692ae55a91d13e5b8d9510cb433706777f
Carlolux <carlos.cg170404@gmail.com>
2024-01-02 17:12:05 -0500

    new menu

--- resources/menu.php
+++ resources/menu.php
@@ -1,46 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Administración Sastreria</a>
+    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?php echo $url;?>main.php">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Utilidades
+            Dropdown
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Crear Trabajo de Grado</a></li>
-            <li><a class="dropdown-item" href="#">Asignar Estudiante</a></li>
-            <li><a class="dropdown-item" href="#">Listar Trabajos</a></li>
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Crear Comité de Evaluación</a></li>
-            <li><a class="dropdown-item" href="#">Evaluar</a></li>
-            <li><a class="dropdown-item" href="#">Listar evaluaciones</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Reportes
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Reporte 1</a></li>
-            <li><a class="dropdown-item" href="#">Reporte 2</a></li>
-            <li><a class="dropdown-item" href="#">Reporte 3</a></li>
-          </ul>
-        </li>
-
         <li class="nav-item">
-          <a class = "nav-link" aria-current = "page" href = "#">Cerrar sesión</a>
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-  
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
   </div>
 </nav>
