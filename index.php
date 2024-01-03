88ffc1fe52348487f08ff97e94d8b449f8d08d96
DieGod123 <diegodelgado685@gmail.com>
2024-01-03 00:46:17 -0500

    Implementación de login básico junto con divs de i nformacíon / Implementación de fuente roboto / dis tribución de elementos y aplicación de algunos es tilos css solo para la información/ Queda a discucio n el posicionamiento de los elementos/colores/conteni do.

--- index.php
+++ index.php
@@ -1,21 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php include("resources/header.php"); ?>
 </head>
+
 <body>
     <?php include("resources/menu.php"); ?>
-    <div class="container">
-        <br>
-        <h2>Bienvenido!</h2>
-        <div class="info">
 
+    <div class="container d-flex justify-content-center align-items-center" style="min-height: 90vh;">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="info text-center">
+                    <h1>¡Bienvenid@ a</h1>
+                    <h1> Sastrería Sur!</h1>
+                    <br>    
+                    <div class="info">
+                        <div class="candidatos">
+                            <div class="content">
+                                <span><ion-icon name="create-outline"></ion-icon>  Explora las funciones intuitivas de nuestro software para gestionar de manera eficiente tus usuarios y proyectos.</span> <br>
+                                <span><ion-icon name="accessibility-outline"></ion-icon>   Ajusta patrones y experimenta con estilos únicos. La confección nunca fue tan precisa y emocionante.</span><br>
+                                <span><ion-icon name="newspaper-outline"></ion-icon> Administra tus servicios de alquiler de manera eficiente con nuestra plataforma.</span>
+                                <!-- <span><ion-icon name="refresh-outline"></ion-icon>  Mantén actusadddddddddddddalizado tu CV</span> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        .
 
+            <div class="col-md-6">
+                <div id="registro" class="text-center p-4 border" style="background-color: #212529; color: white; Border-radius: 15px">
+                    <h2>Inicio de Sesión</h2>
+                    <br>
+                    <form>
+                        <div class="mb-3">
+                            <label for="correo" class="form-label text-white">Correo Electrónico</label>
+                            <input type="email" class="form-control" id="correo" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="contraseña" class="form-label text-white">Contraseña</label>
+                            <input type="password" class="form-control" id="contraseña" required>
+                        </div>
+                        <button type="submit" class="btn btn-dark btn-block">Iniciar Sesión</button>
+                    </form>
+                    <br>
+                    <a href="#" class="mt-2 text-white">Crear cuenta</a>
+                </div>
+            </div>
+        </div>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
-    </body>
+</body>
+
 </html>
