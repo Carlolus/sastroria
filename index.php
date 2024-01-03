656afae4599ef3df01a2de9f11217830b9da1265
DieGod123 <diegodelgado685@gmail.com>
2024-01-02 23:07:25 -0500

    Asigancion de acciónamiento principal en la barra de navegación / Estilo y definición de logo

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
 <body>
     <?php include("resources/menu.php"); ?>
     <div class="container">
-        <h2>Hola Jhonathan</h2>
+        <br>
+        <h2>Bienvenido!</h2>
+        <div class="info">
+
+        </div>
+        .
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
