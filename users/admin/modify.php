3619fb0be30592d6ae94b5212587146c4a921025
Carlolux <carlos.cg170404@gmail.com>
2024-01-03 19:40:52 -0500

    modify issue fixed

--- users/admin/modify.php
+++ users/admin/modify.php
@@ -41,6 +41,9 @@
 
             <button type="submit" class="btn btn-primary">Actualizar</button>
         </form>
-    </div>   
+    </div> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    </body>  
 </body>
 </html>
