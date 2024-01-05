01aab9a00a7112eed046428964df6099314b9241
Carlolux <carlos.cg170404@gmail.com>
2024-01-05 00:45:24 -0500

    create_customer

--- users/customers/create_customer.php
+++ users/customers/create_customer.php
@@ -34,157 +34,159 @@
         }
     </style>
     
-    <div class="container-fluid">
-        <div class="container">
-            <!-- Title -->
-            <div class="d-flex justify-content-between align-items-lg-center py-3 flex-column flex-lg-row">
-                <h2 class="h5 mb-3 mb-lg-0"><a href="../../pages/admin/customers.html" class="text-muted">
-                    <i class="bi bi-arrow-left-square me-2"></i></a>Añadir nuevo cliente</h2>
-                <div class="hstack gap-3">
-                    <button class="btn btn-light btn-sm btn-icon-text"><i class="bi bi-x"></i> <span
-                            class="text">Cancelar</span></button>
-                    <button class="btn btn-primary btn-sm btn-icon-text"><i class="bi bi-save"></i> <span
-                            class="text">Guardar</span></button>
-                </div>
-            </div>
-
-            <!-- Main content -->
-            <div class="row">
-                <!-- Left side -->
-                <div class="col-lg-8">
-                    <!-- Basic information -->
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <h3 class="h6 mb-4">Información Básica</h3>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="mb-3">
-                                        <label class="form-label">Cédula</label>
-                                        <input type="number" class="form-control" min = "1" required>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="mb-3">
-                                        <label class="form-label">Nombre</label>
-                                        <input type="text" class="form-control" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="mb-3">
-                                        <label class="form-label">Teléfono</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-lg-6"> 
-                                    <div class="mb-3">
-                                        <label class="form-label">Dirección</label>
-                                        <input type="text" class="form-control" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="mb-3">
-                                    <label class="form-label">Correo Electrónico:</label>
-                                    <input type="email" class="form-control" >
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <!-- Address -->
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <h3 class="h6 mb-4">Medidas pantalón:</h3>
-                            <div class="row">
-                                <div class="col-lg-3">
-                                    <label class="form-label">Largo</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                                <div class="col-lg-3">
-                                    <label class="form-label">Tiro</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                                <div class="col-lg-3">
-                                    <label class="form-label">Cintura</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                                <div class="col-lg-3">
-                                    <label class="form-label">Base</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-3">
-                                    <label class="form-label">Pierna</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                                <div class="col-lg-3">
-                                    <label class="form-label">Rodilla</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                                <div class="col-lg-3">
-                                    <label class="form-label">Bota</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                            </div>
-                        </div>
+    <form action="add_customer.php" id = "frmAddCustomer">
+        <div class="container-fluid">
+            <div class="container">
+                <!-- Title -->
+                <div class="d-flex justify-content-between align-items-lg-center py-3 flex-column flex-lg-row">
+                    <h2 class="h5 mb-3 mb-lg-0"><a href="../../pages/admin/customers.html" class="text-muted">
+                        <i class="bi bi-arrow-left-square me-2"></i></a>Añadir nuevo cliente</h2>
+                    <div class="hstack gap-3">
+                        <button class="btn btn-light btn-sm btn-icon-text"><i class="bi bi-x"></i> <span
+                                class="text">Cancelar</span></button>
+                        <button class="btn btn-dark btn-sm btn-icon-text" type="submit"><i class="bi bi-save"></i> <span class="text">Guardar</span></button>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <h3 class="h6">Medidas chaleco</h3>
-                            <label class="form-label">Largo</label>
-                            <input type="number" class="form-control">
-                            <br>
-                            <label class="form-label">Talle</label>
-                            <input type="number" class="form-control">
-                            <br>
-                            <label class="form-label">Alto</label>
-                            <input type="number" class="form-control">
+
+                <!-- Main content -->
+                <div class="row">
+                    <!-- Left side -->
+                    <div class="col-lg-8">
+                        <!-- Basic information -->
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h3 class="h6 mb-4">Información Básica</h3>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Cédula</label>
+                                            <input id = "txtId" name = "txtId" type="number" class="form-control" min = "1" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Nombre</label>
+                                            <input id = "txtName" name = "txtName" type="text" class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="mb-3">
+                                            <label class="form-label">Teléfono</label>
+                                            <input id = "txtPhone" name = "txtPhone" type="text" class="form-control" required min = "0">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6"> 
+                                        <div class="mb-3">
+                                            <label class="form-label">Dirección</label>
+                                            <input id = "txtAdress" name = "txtAdress" type="text" class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="mb-3">
+                                        <label class="form-label">Correo Electrónico:</label>
+                                        <input id = "txtEmail" name = "txtEmail" type="email" class="form-control">
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!-- Address -->
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h3 class="h6 mb-4">Medidas pantalón</h3>
+                                <div class="row">
+                                    <div class="col-lg-3">
+                                        <label class="form-label">Largo</label>
+                                        <input id="txtLargoP" name="txtLargoP" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label class="form-label">Tiro</label>
+                                        <input id="txtTiroP" name="txtTiroP" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label class="form-label">Cintura</label>
+                                        <input id="txtCinturaP" name="txtCinturaP" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label class="form-label">Base</label>
+                                        <input id="txtBaseP" name="txtBaseP" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-3">
+                                        <label class="form-label">Pierna</label>
+                                        <input id="txtPiernaP" name="txtPiernaP" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label class="form-label">Rodilla</label>
+                                        <input id="txtRodillaP" name="txtRodillaP" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label class="form-label">Bota</label>
+                                        <input id="txtBotaP" name="txtBotaP" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <h3 class="h6">Medidas Saco</h3>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <label class="form-label">Largo</label>
-                                    <input type="number" class="form-control">
+                    <div class="col-lg-4">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h3 class="h6">Medidas chaleco</h3>
+                                <label class="form-label">Largo</label>
+                                <input id="txtLargoV" name="txtLargoV" type="number" class="form-control" min="0" max="999">
+                                <br>
+                                <label class="form-label">Talle</label>
+                                <input id="txtTalleV" name="txtTalleV" type="number" class="form-control" min="0" max="999">
+                                <br>
+                                <label class="form-label">Pecho</label>
+                                <input id="txtPechoV" name="txtPechoV" type="number" class="form-control" min="0" max="999">
+                            </div>
+                        </div>
+
+                        <!-- Medidas Saco -->
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h3 class="h6">Medidas Saco</h3>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <label class="form-label">Largo</label>
+                                        <input id="txtLargoS" name="txtLargoS" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label class="form-label">Talle</label>
+                                        <input id="txtTalleS" name="txtTalleS" type="number" class="form-control" min="0" max="999">
+                                    </div>
                                 </div>
-                                <div class="col-lg-6">
-                                    <label class="form-label">Talle</label>
-                                    <input type="number" class="form-control">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <label class="form-label">Espalda</label>
+                                        <input id="txtEspaldaS" name="txtEspaldaS" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label class="form-label">Hombro</label>
+                                        <input id="txtHombroS" name="txtHombroS" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <label class="form-label">Pecho</label>
+                                        <input id="txtPechoS" name="txtPechoS" type="number" class="form-control" min="0" max="999">
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label class="form-label">Manga</label>
+                                        <input id="txtMangaS" name="txtMangaS" type="number" class="form-control" min="0" max="999">
+                                    </div>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <label class="form-label">Espalda</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                                <div class="col-lg-6">
-                                    <label class="form-label">Hombro</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <label class="form-label">Pecho</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                                <div class="col-lg-6">
-                                    <label class="form-label">Manga</label>
-                                    <input type="number" class="form-control">
-                                </div>
-                            </div>
-                            
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </form> 
 </body>
